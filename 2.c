71ec7cecc1c4dcdf819e164d578162e8d0030f53
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2020-06-24 16:06:27 -0600

    Update 2.c

--- 2.c
+++ 2.c
@@ -4,15 +4,14 @@ struct complejo{
 	float real,imaginaria;
 }z1,z2;
 
+
 void pedirDatos();
 void suma(struct complejo,struct complejo);
-void muestra(struct complejo);
-
-
+void multiplicacion(struct complejo,struct complejo);
 
 int main(){
 	pedirDatos();
-	suma(z1,z2);
+	multiplicacion(z1, z2);
 	return 0;
 } 
 
@@ -27,14 +26,17 @@ void pedirDatos(){
 	printf("Digite la parte imaginaria: "); scanf("%f", &z2.imaginaria);
 }
 
-void suma(struct complejo z1,struct complejo z2){
-	z1.real = (z1.real + z2.real) - (z1.imaginaria + z2.imaginaria);
-	z1.imaginaria += z2.imaginaria;
-	printf("La suma es: %f , %f j", z1.real, z1.imaginaria);
+void multiplicacion(struct complejo z1,struct complejo z2){
+  float real = (z1.real * z2.real) - (z1.imaginaria * z2.imaginaria);
+  float imaginario = (z1.real * z2.imaginaria) + (z2.real * z1.imaginaria);
+	z1.real = real;
+	z1.imaginaria = imaginario;
+	printf("La multiplicacion es: %f , %f j", z1.real, z1.imaginaria);
 }
 
-void multiplicacion(struct complejo z1,struct complejo z2){
+void suma(struct complejo z1,struct complejo z2){
 	z1.real += z2.real;
 	z1.imaginaria += z2.imaginaria;
 	printf("La suma es: %f , %f j", z1.real, z1.imaginaria);
 }
+
