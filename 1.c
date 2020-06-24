3008b4219cba3e001e298f5839dd70bd04dd4db4
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2020-06-24 15:09:58 -0600

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 int main(){
-  double var1 = 0, n = 1, x = 0.3; 
+  double var1 = 0, n = 1, x = 0.7; 
   do{
     n *= 2;
-    var1 = 1/(x*n);
-  }while(1/(x++*n) < 0.0000000001);
+    var1 += 1/(x*n);
+  }while(!(1/(x++*n) < 0.0000000001));
 
 printf("serie = %.10lf\n", var1);
 
