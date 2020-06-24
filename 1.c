58c352f9d5eebca2ef7c83d54821579db2342f65
David Vallecios <37277365+DavidVallecios@users.noreply.github.com>
2020-06-24 14:38:38 -0600

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  double var1 = 0, n = 1, x = 0.3; 
+  do{
+    n *= 2;
+    var1 = 1/(x*n);
+  }while(1/(x++*n) < 0.0000000001);
+
+printf("serie = %.10lf\n", var1);
+
+return 0;
+}
